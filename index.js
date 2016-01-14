cf2834055e960a0010b2d6a016a30bcb5ee1c088
xtina-starr <c.thompsonms@gmail.com>
2016-01-14 14:52:25 -0500

    added comma in index file

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = {
   Image: require("./out/image"),
   Feature: require("./out/feature"),
   CurrentUser: require("./out/current_user"),
-  ArtworkHelpers: require("./out/artwork_helpers")
+  ArtworkHelpers: require("./out/artwork_helpers"),
   CalendarUrls: require("./out/calendar_urls")
 }
